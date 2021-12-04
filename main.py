8698dd545c2f047c177a8e41b523d25e8673875a
SebastianAguirre17 <sebaaguirre2012@gmail.com>
2021-12-03 23:07:56 -0300

    Correcciones, simplificacion de codigo

--- main.py
+++ main.py
@@ -73,20 +73,23 @@ def solicita_tipos_situaciones():
     letrasIngresadas = ''
     contador = 0
     flag = True
-    print("Tipos de situaciones vivenciadas:")
-    print("a) Hechos de violencia sexual \nb) Hechos de acoso sexual \nc) Hechos con connotación sexista \nd) Comportamientos y acciones de violencia")
+    print("\nTipos de situaciones vivenciadas:\n")
+    print("a) Hechos de violencia sexual")
+    print("b) Hechos de acoso sexual")
+    print("c) Hechos con connotación sexista")
+    print("d) Comportamientos y acciones de violencia\n")
     while flag:
         tipo = input("Elija una opción: ")
         
         if tipo != 'a' and tipo != 'b' and tipo != 'c' and tipo != 'd':
-            print("La opción ingresada es incorrecta.")
+            print("¡La opción ingresada es incorrecta!")
         else:
             if tipo in letrasIngresadas:
                 print("La opción fue elegida con anterioridad.")
             else: 
                 letrasIngresadas = letrasIngresadas + tipo
                 tipos = tipos + devuelve_situaciones(tipo) + " - "
-                eleccion = input("¿Desea ingresar otra opción? S/N: ")
+                eleccion = input("\n¿Desea ingresar otra opción? S/N: ")
                 contador = contador + 1
                 if eleccion == 'N' or eleccion == 'n':
                     flag = False
@@ -100,15 +103,19 @@ def mostrar_denuncia(anio, nro_exp, dia, mes, genero_denunciante, claustro_denun
     """ Recibe por parámetro datos de la denuncia y los imprime en pantalla
         de forma descriptiva y ordenada.
     """
-    os.system('cls')
-    print("Datos de la denuncia:")
-    print("Número de expediente:", nro_exp)
-    print("Fecha de denuncia:", dia, "/", mes, "/", anio)
-    print("Género auto percibido de la persona denunciante:", devuelve_genero(genero_denunciante))
-    print("Claustro al que pertenece la persona denunciante:", devuelve_claustro(claustro_denunciante))
-    print("Tipo/s de situación/es vivenciada/s:", tipos_situaciones)
-    print("Género de la persona denunciada:", devuelve_genero(genero_denunciado))
-    print("Claustro de la persona denunciada:", devuelve_claustro(claustro_denunciado))
+    fecha = str(dia) + "/" + str(mes) + "/" + str(anio)
+    print("\n----------------------------------------------------------------------------------")
+    print("--   Datos de la denuncia:")
+    print("----------------------------------------------------------------------------------")
+    print("--   Número de expediente:               ", nro_exp)
+    print("--   Fecha de denuncia:                  ", fecha)
+    print("--   Género de la persona denunciante:   ", devuelve_genero(genero_denunciante))
+    print("--   Claustro de la persona denunciante: ", devuelve_claustro(claustro_denunciante))
+    print("--   Tipo/s de situación/es vivenciada/s:", tipos_situaciones)
+    print("--   Género de la persona denunciada:    ", devuelve_genero(genero_denunciado))
+    print("--   Claustro de la persona denunciada:  ", devuelve_claustro(claustro_denunciado))
+    print("----------------------------------------------------------------------------------")
+    
     input("Presione Enter para continuar.")
 
 def mostrar_estadisticas(anio, semestre, total_denuncias, porcentaje, cant_pares, mayor_exp, cant_den_mujeres, cant_den_varones, 
@@ -116,71 +123,78 @@ def mostrar_estadisticas(anio, semestre, total_denuncias, porcentaje, cant_pares
     """ Recibe por parámetro estadísticos y los imprime en pantalla
         de forma descriptiva y ordenada.
     """
-    os.system('cls')
-    print("Informe del semestre", semestre, "del año", anio)
-    print("Cantidad total de denuncias:", total_denuncias)
-    print("Porcentaje de clasificadas en más de un tipo de situación vivenciada:", porcentaje, "%")
-    print("Cantidad de denuncias entre pares del mismo claustro:", cant_pares)
-    print("Mayor número de expediente ingresado:", mayor_exp)
-    print("Cantidad de denunciantes mujeres:", cant_den_mujeres)
-    print("Cantidad de denunciantes varones:", cant_den_varones)
-    print("Cantidad de denunciantes otres:", cant_den_otres)
-    print("Cantidad de denunciantes del claustro de estudiantes:", cant_den_estudiantes)
-    print("Cantidad de denunciantes del claustro de no docentes:", cant_den_nodocentes)
-    print("Cantidad de denunciantes del claustro de docentes:", cant_den_docentes)
-    print("Cantidad de denunciantes del claustro de graduades:", cant_den_graduades)
-    input("Presione Enter para continuar.")
+    print("\n---------------------------------------------------------------------")
+    print("--   Informe del semestre", semestre, "del año", anio)
+    print("---------------------------------------------------------------------")
+    print("--  Total de denuncias:                                    ", total_denuncias)
+    print("--  Clasificadas en más de un tipo de situación vivenciada:", porcentaje, "%")
+    print("--  Denuncias entre pares del mismo claustro:              ", cant_pares)
+    print("--  Mayor número de expediente ingresado:                  ", mayor_exp)
+    print("--  Denunciantes mujeres:                                  ", cant_den_mujeres)
+    print("--  Denunciantes varones:                                  ", cant_den_varones)
+    print("--  Denunciantes otres:                                    ", cant_den_otres)
+    print("--  Denunciantes del claustro de estudiantes:              ", cant_den_estudiantes)
+    print("--  Denunciantes del claustro de no docentes:              ", cant_den_nodocentes)
+    print("--  Denunciantes del claustro de docentes:                 ", cant_den_docentes)
+    print("--  Denunciantes del claustro de graduades:                ", cant_den_graduades)
+    print("---------------------------------------------------------------------")
+    input("\nPresione Enter para continuar.")
 
 def main():
+    print("-----------------------------------------------------")
+    print("--   Programa Transversal de Políticas de Género   --")
+    print("--          y Diversidad de la UNDAV               --")
+    print("-----------------------------------------------------")
+    
     anio = 0
     while anio < 2021:
-        anio = int(input("Ingrese en año del informe: "))
+        anio = int(input("\nIngrese año del informe: "))
         if anio < 2021:
-            print("El año ingresado es incorrecto! Debe ser mayor a 2020")
+            print("¡Año Incorrecto! Debe ser mayor a 2020.")
     
     semestre = 0
     while semestre != 1 and semestre != 2:
-        semestre = int(input("Ingrese en semestre del informe: "))
+        semestre = int(input("Ingrese semestre del informe: "))
         if semestre != 1 and semestre != 2:
-            print("El semestre ingresado es incorrecto!")
+            print("¡Semestre incorrecto!")
     
     cant_den_mujeres = cant_den_varones = cant_den_otres = cant_den_estudiantes = cant_den_nodocentes = 0
     cant_den_docentes = cant_den_graduades = mayor_exp = cant_pares = acum_varios_tipos = 0
     
     continuar = 'S'
     while continuar == 'S' or continuar == 's':
-        continuar = input("Presione 'S' para ingresar una denuncia, cualquier otra letra para continuar: ")
+        continuar = input("\nPresione 'S' para ingresar una denuncia o 'N' para continuar: ")
         if continuar == 'S' or continuar == 's':
             # EXPEDIENTE
-            expediente = int(input("Ingrese el número de expediente: "))
+            expediente = int(input("\nIngrese número de expediente: "))
             if expediente > mayor_exp:
                 mayor_exp = expediente
             
             # MES
             mes_valido = False
             while not mes_valido:
-                mes = int(input("Ingrese el mes de la denuncia: "))
+                mes = int(input("Ingrese mes de la denuncia: "))
                 if mes > 12 or mes < 1:
-                    print("El mes ingresado es incorrecto! Reintente.") 
+                    print("¡Mes incorrecto! Reintente.") 
                 elif (semestre == 1 and mes > 6) or (semestre == 2 and mes < 7):
-                    print("El mes no se corresponde con el semestre! Reintente.")
+                    print("¡El mes no pertenece al semestre! Reintente.")
                 else:
                     mes_valido = True
             
             # FECHA
             fecha_valida = False
             while not fecha_valida:
-                dia = int(input("Ingrese el dia de la denuncia: "))
+                dia = int(input("Ingrese dia de la denuncia: "))
                 fecha_valida = valida_fecha(dia, mes, anio)
                 if not fecha_valida:
-                    print("El día ingresado es incorrecto! Reingrese día")
+                    print("Fecha incorrecta! Reingrese día.")
 
             # GENERO DENUNCIANTE
             genero_denunciante = '-'
             while genero_denunciante != 'm' and genero_denunciante != 'v' and genero_denunciante != 'x':
-                genero_denunciante = input("Ingrese el género autopercibido de la persona denunciante (x - m - v): ")
+                genero_denunciante = input("Ingrese el género de la persona denunciante (x - m - v): ")
                 if genero_denunciante != 'm' and genero_denunciante != 'v' and genero_denunciante != 'x':
-                    print("El dato ingresado es incorrecto! Reingrese el género.")
+                    print("¡El dato ingresado es incorrecto! Reingrese el género.")
             if genero_denunciante == 'm':
                 cant_den_mujeres = cant_den_mujeres + 1  
             elif genero_denunciante == 'v':
@@ -191,9 +205,9 @@ def main():
             #CLAUSTRO DENUNCIANTE
             claustro_denunciante = '-'
             while claustro_denunciante != 'e' and claustro_denunciante != 'n' and claustro_denunciante != 'd' and claustro_denunciante != 'd':
-                claustro_denunciante = input("Ingrese el claustro al que pertenece de la persona denunciante (e, n, d, g): ")
+                claustro_denunciante = input("Ingrese el claustro de la persona denunciante (e, n, d, g): ")
                 if claustro_denunciante != 'e' and claustro_denunciante != 'n' and claustro_denunciante != 'd' and claustro_denunciante != 'd':
-                    print("El dato ingresado es incorrecto! Reingrese el claustro.")
+                    print("¡El dato ingresado es incorrecto! Reingrese el claustro.")
             if claustro_denunciante == 'e':
                 cant_den_estudiantes = cant_den_estudiantes + 1  
             elif claustro_denunciante == 'n':
@@ -211,16 +225,16 @@ def main():
             # GENERO DENUNCIADO
             genero_denunciado = '-'
             while genero_denunciado != 'm' and genero_denunciado != 'v' and genero_denunciado != 'x':
-                genero_denunciado = input("Ingrese el género autopercibido de la persona denunciada (x - m - v): ")
+                genero_denunciado = input("Ingrese el género de la persona denunciada (x - m - v): ")
                 if genero_denunciado != 'm' and genero_denunciado != 'v' and genero_denunciado != 'x':
-                    print("El dato ingresado es incorrecto! Reingrese el género.")
+                    print("¡El dato ingresado es incorrecto! Reingrese el género.")
             
             #CLAUSTRO DENUNCIADO
             claustro_denunciado = '-'
             while claustro_denunciado != 'e' and claustro_denunciado != 'n' and claustro_denunciado != 'd' and claustro_denunciado != 'g':
-                claustro_denunciado = input("Ingrese el claustro ala que pertenece de la persona denunciada (e, n, d, g): ")
+                claustro_denunciado = input("Ingrese el claustro de la persona denunciada (e, n, d, g): ")
                 if claustro_denunciado != 'e' and claustro_denunciado != 'n' and claustro_denunciado != 'd' and claustro_denunciado != 'g':
-                    print("El dato ingresado es incorrecto! Reingrese el claustro.")
+                    print("¡El dato ingresado es incorrecto! Reingrese el claustro.")
     
             if claustro_denunciado == claustro_denunciante:
                 cant_pares = cant_pares + 1
@@ -229,10 +243,16 @@ def main():
         # FIN BUCLE
 
     total_denuncias = cant_den_mujeres + cant_den_varones + cant_den_otres
-    porcentaje = cant_pares * 100 / total_denuncias
+    porcentaje = 0
+    if acum_varios_tipos != 0:
+        porcentaje = acum_varios_tipos * 100 / total_denuncias
     
     mostrar_estadisticas(anio, semestre, total_denuncias, porcentaje, cant_pares, mayor_exp, cant_den_mujeres, cant_den_varones, 
                          cant_den_otres, cant_den_docentes, cant_den_nodocentes, cant_den_estudiantes, cant_den_graduades)
-
 main()
 
+
+# PRUEBA DE FUNCIONES
+# mostrar_denuncia(2021, 12345678, 2, 2, 'x', 'e', 'm', 'e', 'Hechos de violencia sexual Hechos de acoso sexual')
+# mostrar_estadisticas(2021, 1, 2, 50, 2, 12345678, 2, 4, 0, 2, 3, 6, 8)
+# solicita_tipos_situaciones()
